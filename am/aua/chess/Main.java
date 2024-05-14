fb984cbc55513dbb3b5ce52671d999532abd7c06
zakaryan2107 <zakaryan2107@...>
2024-05-14 22:06:47 +0400

    add

--- am/aua/chess/Main.java
+++ am/aua/chess/Main.java
@@ -5,15 +5,16 @@ import am.aua.chess.core.IllegalArrangementException;
 import am.aua.chess.ui.ChessUI;
 
 public class Main {
-public static void main(String[] args) throws IllegalArrangementException {
-    if (args.length == 1 && args[0].equals("-console")) {
-        ChessConsole console = new ChessConsole();
-        console.run();
-    } else {
-        ChessUI gui = new ChessUI();
-    }
-}
 
+    public static void main(String[] args) throws IllegalArrangementException {
+        
+        if (args.length == 1 && args[0].equals("-console")) {
+            ChessConsole console = new ChessConsole();
+            console.run();
+        } else {
+            ChessUI gui = new ChessUI();
+        }
+    }
 }    
 
 
